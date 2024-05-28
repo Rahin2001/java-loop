1c361d4796750284b99fd66e7c0f5e3dab015b16
Rahul Murali <rahulmuralitechnology@gmail.com>
2024-05-28 19:38:46 +0530

    java

---
+++ ll.js
@@ -0,0 +1,14 @@
+for (let i = 1; i <= 5; i++) {
+    let stars = '';
+    for (let j = 0; j < i; j++) {
+        stars += '*';
+    }
+    console.log(stars);
+}
+for (let i = 4; i >= 1; i--) {
+    let stars = '';
+    for (let j = 0; j < i; j++) {
+        stars += '*';
+    }
+    console.log(stars);
+}
